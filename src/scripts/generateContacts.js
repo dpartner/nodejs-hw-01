185dbc3f4bdd322797386ec6d8ee48718be2fa64
dpartner <d.partner.ua@gmail.com>
2024-11-11 23:23:25 +0200

    done

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,16 @@
-const generateContacts = async (number) => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+const generateContacts = async (number) => {
+  const contacts = await readContacts();
+
+  const createdContacts = [];
+  for (let i = 0; i < number; i += 1) {
+    createdContacts.push(createFakeContact());
+  }
+  const newContacts = [...contacts, ...createdContacts];
+  await writeContacts(newContacts).catch((err) => console.log(err));
+};
 
 generateContacts(5);
--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,11 @@
-export const getAllContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+
+export const getAllContacts = async () => {
+  const contactsArr = await readContacts();
+  if (contactsArr.length === 0) {
+    return 'Contacts not found';
+  }
+  return contactsArr;
+};
 
 console.log(await getAllContacts());
